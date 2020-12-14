74805631163dbf603022bd32d319c3f700fe7b0e
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-14 15:50:49 +0330

    Update e9.py

--- e9.py
+++ e9.py
@@ -42,16 +42,16 @@ def __target__():
             pass
     if not "http" in target or not "https" in target:
        target = "http://" + target
-    ip12 = socket.gethostbyname(target)
+    mn = socket.gethostbyname(target)
     r = requests.get(target)
     coun = 1
     time = datetime.datetime.now()
     while True:
         coun = 1
         if r.status_code == 404 or r.status_code == 500:
-            print(Fore.BLUE + "[ " , Fore.RED + coun , Fore.BLUE + " ]" + Fore.RED + " ~ " + Fore.RED + target + Fore.GREEN + " ==>  " , Fore.RED + ip12 , Fore.YELLOW + time)
+            print(Fore.BLUE + "[ " , Fore.RED + coun , Fore.BLUE + " ]" + Fore.RED + " ~ " + Fore.RED + target + Fore.GREEN + " ==>  " , Fore.RED + mn , Fore.YELLOW + time)
             coun += 1
         if r.status_code == 200:
-            print(Fore.BLUE + "[ " , Fore.GREEEN +  coun , Fore.BLUE + " ]" + Fore.GREEEN + " ~ " + Fore.GREEEN + target + Fore.RED + " ==> " ,Fore.GREEN + ip12 , Fore.YELLOW + time)
+            print(Fore.BLUE + "[ " , Fore.GREEEN +  coun , Fore.BLUE + " ]" + Fore.GREEEN + " ~ " + Fore.GREEEN + target + Fore.RED + " ==> " ,Fore.GREEN + mn , Fore.YELLOW + time)
             coun += 1
 __target__()
